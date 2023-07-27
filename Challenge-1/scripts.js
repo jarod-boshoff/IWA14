3f6a9d85984848eed9e976b43192855923e4608d
jarod boshoff <boso.hoff@gmail.com>
2023-07-27 22:25:32 +0200

    updated-challenge-1

---
+++ Challenge-1/scripts.js
@@ -0,0 +1,13 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = (parameter) => {
+  console.log(parameter)
+  console.log(parameter)
+}
+
+function logHobby() {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+logHobby()
